eda1691f1fa8c49ef640652796e708fd48aa958a
CelalBerkeAkyol <buscberke@gmail.com>
2024-12-30 14:44:19 +0300

    Daha iyi auth token sonuçları dönüyor

--- controllers/authController.js
+++ controllers/authController.js
@@ -118,14 +118,14 @@ const verifyToken = async (req, res) => {
   const token = req.cookies.token;
 
   if (!token) {
-    return res.status(401).json({ error: "Token bulunamadı." });
+    return res.status(401).json({ valid: false, error: "Token bulunamadı." });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    res.status(200).json({ valid: true, user: decoded });
-  } catch (err) {
-    res.status(401).json({ error: "Geçersiz token." });
+    return res.status(200).json({ valid: true, user: decoded });
+  } catch (error) {
+    return res.status(401).json({ valid: false, error: "Geçersiz token." });
   }
 };
 const logout = async (req, res) => {
