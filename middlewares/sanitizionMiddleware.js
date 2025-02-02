ea9d9880f57c6c2514391df69c7b7ebe57990d7c
CelalBerkeAkyol <buscberke@gmail.com>
2025-02-02 21:18:08 +0300

    loglama eklendi

--- middlewares/sanitizionMiddleware.js
+++ middlewares/sanitizionMiddleware.js
@@ -1,10 +1,11 @@
+// /middlewares/sanitizePostContent.js
 const sanitizeHtml = require("sanitize-html");
 
 const sanitizePostContent = (req, res, next) => {
   if (req.body.content) {
+    console.info("sanitizePostContent: Post içeriği sanitize ediliyor.");
     req.body.content = sanitizeHtml(req.body.content);
   }
-
   next();
 };
 
