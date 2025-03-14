b608db320a27bcfd5031020906743f1a24a49b93
CelalBerkeAkyol <buscberke@gmail.com>
2025-03-14 12:01:29 +0300

    Bug fix - Function name change corrected.

--- Routers/categoryRouter.js
+++ Routers/categoryRouter.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const {
   getPostsByCategoriesName,
+  getAllCategories,
 } = require("../controllers/categoryController");
-const { getAllCategory } = require("../controllers/categoryController");
+
 // kategori ismine göre postları döndüren api
-router.get("/all-categories", getAllCategory);
+router.get("/all-categories", getAllCategories);
 router.get("/:category", getPostsByCategoriesName);
 // postların kategorilerini
 
