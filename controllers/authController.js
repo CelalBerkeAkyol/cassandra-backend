0c7a313981866a297a30932be50a24f1985a4d70
iAmNsengi <iamnsengi@icloud.com>
2025-04-01 17:38:45 +0200

    fx: bcrypt to bcryptjs && add verificationString and expiration to UserSchema

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../Models/UserSchema");
 const { clearAuthCookies } = require("../Helpers/tokenHelpers");
 
