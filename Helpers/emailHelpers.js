0e0bff9828e801082d7b4458dda9fd6591d07bc6
iAmNsengi <iamnsengi@icloud.com>
2025-04-02 10:10:20 +0200

    fx: transporter configuration

--- Helpers/emailHelpers.js
+++ Helpers/emailHelpers.js
@@ -1,40 +1,36 @@
 const nodemailer = require("nodemailer");
 const crypto = require("crypto");
 
-const sendVerificationEmail = async (user, res) => {
+const sendVerificationEmail = async (user) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      service: "Gmail",
       auth: {
-        email: process.env.GOOGLE_MAIL,
-        password: process.env.GOOGLE_PASS,
+        user: process.env.GOOGLE_MAIL,
+        pass: process.env.GOOGLE_PASS,
       },
     });
 
     const token = crypto.randomBytes(32).toString("hex");
+    user.verificationToken = token;
+    user.verificationTokenExpiresAt = Date.now() + 2 * 3600000; // 2 hours
 
     const mailOptions = {
       from: process.env.GOOGLE_MAIL,
       to: user.email,
       subject: "Verify your Email | Fin AI",
-      text: `Please verify your email by clicking this link : \n${
+      text: `Please verify your email by clicking this link: \n${
         process.env.FRONTEND_URL || "http://localhost:5173"
       }/verify-email?token=${token}\n\nLink expires in 2 hours\n\nFin Ai`,
     };
 
-    user.verificationToken = token;
-    user.verificationTokenExpiresAt = Date.now() + 2 * 36000000; // 2 hours
-
-    await transporter.sendMail(mailOptions, (err, res) => {
-      if (err) throw new Error("Sending verification email failed");
-    });
+    await transporter.sendMail(mailOptions);
     await user.save();
+
+    return { token, expiresAt: user.verificationTokenExpiresAt };
   } catch (error) {
-    console.error("auth/sendVerification Email hata:", error);
-    res.status(400).json({
-      success: false,
-      message: "Doğrulama e-postası gönderilemedi",
-    });
+    console.error("Error sending verification email:", error);
+    throw new Error("Doğrulama e-postası gönderilemedi");
   }
 };
 
