d2026c3b887697e513c5636af869164e32f4a026
CelalBerkeAkyol <buscberke@gmail.com>
2025-04-02 12:28:30 +0300

    Enhance: Increased database connection timeout durations

--- Helpers/connectDatabase.js
+++ Helpers/connectDatabase.js
@@ -2,7 +2,14 @@ const mongoose = require("mongoose");
 
 const conntectToDatabase = () => {
   mongoose
-    .connect(process.env.MONGOOSE_URL)
+    .connect(process.env.MONGOOSE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 60000,
+      socketTimeoutMS: 90000,
+      retryWrites: true,
+      w: "majority",
+    })
     .then(() => {
       console.log("Connection is succesfull");
     })
