97edcbd1575776191c69a8b88a5bd5525aaf658b
CelalBerkeAkyol <buscberke@gmail.com>
2025-04-03 17:46:55 +0300

    Chore: Cleanup and refinements after merge

--- Helpers/connectDatabase.js
+++ Helpers/connectDatabase.js
@@ -3,12 +3,8 @@ const mongoose = require("mongoose");
 const conntectToDatabase = () => {
   mongoose
     .connect(process.env.MONGOOSE_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 60000,
       socketTimeoutMS: 90000,
-      retryWrites: true,
-      w: "majority",
     })
     .then(() => {
       console.log("Connection is succesfull");
